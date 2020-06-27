381880e24d3112f3efc6ac2f0189e712237e0c41
Cung Nguyen <cungnv2912@gmail.com>
2020-06-26 20:11:04 -0700

    create funtion displayCheck

--- src/Components/SectionClass.js
+++ src/Components/SectionClass.js
@@ -11,7 +11,49 @@ export default class SectionClass extends Component {
   notification=()=>{this.setState({
     isCollapsed:!this.state.isCollapsed
   })}
-  
+
+  renderFooter=()=>(
+    <footer className="footer text-center">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-4 mb-5 mb-lg-0">
+          <h4 className="text-uppercase mb-4">Location</h4>
+          <p className="lead mb-0">
+            2215 John Daniel Drive
+            <br />
+            Clark, MO 65243
+          </p>
+        </div>
+        <div className="col-lg-4">
+          <h4 className="text-uppercase mb-4">About Freelancer</h4>
+          <p className="lead mb-0">
+            Freelance is a free to use, MIT licensed Bootstrap theme created by
+            <a href="http://startbootstrap.com">Start Bootstrap</a>
+            .
+          </p>
+        </div>
+        <div className="col-lg-4">
+          <button className="btn btn-success text-center" onClick={()=>this.notification()}>Edit</button>
+        </div>
+      </div>
+    </div>
+  </footer>
+  )
+
+  renderForm=()=>(
+    <div className="row">
+      <input className="form-control" type="text" placeholder="Edit"/>
+      <button className="btn btn-success" onClick={()=>this.notification()}>Save</button>
+    </div>
+  )
+
+  displayCheck=()=>{
+    if(this.state.isCollapsed===false){
+      return this.renderFooter()
+    }else{
+      return this.renderForm()
+    }
+  }
   render() {
     return (
       <section>
@@ -34,6 +76,7 @@ export default class SectionClass extends Component {
             </div>
           </div>
         </div>
+        {this.displayCheck()}
       </div>
     </section>
     )
