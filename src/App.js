183be9b894dbb661d99da40cbeeaf62ef8e0fcc4
Cung Nguyen <cungnv2912@gmail.com>
2020-06-28 19:45:25 -0700

    learn about life cycle mouting: react23

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import TopMenu from './Components/TopMenu'
 import Header from './Components/Header'
@@ -8,17 +8,36 @@ import Footer from './Components/Footer'
 import img1 from './img/01.jpg'
 import img2 from './img/02.jpg'
 import img3 from './img/03.jpg'
-function App() {
-  return (
-    <div className="App">
-      <TopMenu/>
-      <Header/>
-      <Section order_1="order-lg-1" order_2="order-lg-2" src={img1} title="For those about to rock..."/>
-      <Section src={img2} title="We salute you!"/>
-      <SectionClass order_1="order-lg-1" order_2="order-lg-2" title="Let there be rock!" src={img3}/>
-      <Footer/>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      trangThai:'khoitao'
+    }
+  }
+  
+  componentWillMount() {
+    console.log('componentWillMount dang chay');
+    
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount dang chay')
+  }
+  
+  render(){
+    console.log("render dang chay")
+    return (
+      <div className="App">
+        <TopMenu/>
+        <Header/>
+        <Section order_1="order-lg-1" order_2="order-lg-2" src={img1} title="For those about to rock..."/>
+        <Section src={img2} title="We salute you!"/>
+        <SectionClass order_1="order-lg-1" order_2="order-lg-2" title="Let there be rock!" src={img3}/>
+        <Footer/>
+      </div>
+    );
+  }
 }
 
 export default App;
